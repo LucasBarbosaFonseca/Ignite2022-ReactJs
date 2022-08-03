b0da93a3eabc727408e1475dc35b1055874de232
Lucas Barbosa Fonseca <lufonsecabarbosa@hotmail.com>
2022-08-03 09:32:18 -0300

    sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from './components/Header';
 import { Post } from './Post';
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="Lucas" 
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quaerat sapiente consequuntur minus dolorum qui deserunt molestiae maxime" 
-      />
-      <Post 
-        author="Bereguejonson" 
-        content="Jogles" 
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Lucas"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam quaerat sapiente consequuntur minus dolorum qui deserunt molestiae maxime"
+          />
+          <Post
+            author="Bereguejonson"
+            content="Jogles"
+          />
+        </main>
+      </div>
     </div>
   )
 }
