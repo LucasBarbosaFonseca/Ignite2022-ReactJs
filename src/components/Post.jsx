b62f1ffcd92c51dc8d703df9dc6303cfb5aa7992
Lucas Barbosa Fonseca <lufonsecabarbosa@hotmail.com>
2022-08-24 22:22:50 -0300

    component avatar

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -6,7 +7,7 @@ export function Post() {
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <img className={styles.avatar} src="https://github.com/LucasBarbosaFonseca.png" />
+                    <Avatar src="https://github.com/LucasBarbosaFonseca.png" />
                     <div className={styles.userInfo}>
                         <strong>Lucas</strong>
                         <span>Web Developer</span>
